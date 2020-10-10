8b07b5313c40189ef572a26abafaf490273d4d11
PositivePeriod <positiveperiod.dev@gmail.com>
2020-10-10 18:59:01 +0900

    Update IdentityV
    Consider USD

--- public/game/IdentityV/process.js
+++ public/game/IdentityV/process.js
@@ -1,31 +1,34 @@
-const string_meari = ["60 + 0", "185 + 9", "305 + 15", "690 + 33", "2025 + 98", "3330 + 168", "6590 + 328"];
-const menu_meari = [60, 194, 320, 723, 2123, 3498, 6918];
-const menu_price = [1200, 3900, 5900, 14000, 39000, 65000, 119000];
-const l = menu_meari.length;
-
-var trans_language = 'English'
+var language = 'English'
 var memo_meari = null;
 var memo_n = null;
 var memo_cost = null;
 
-const trans_string = {
-    'English': ['Identity V Echoes calculator', 'How much do you need?', 'Calculate', 'Echoes', 'Cost', 'Amount'],
-    'Korean': ['제5인격 메아리 계산기', '얼마나 필요하신가요?', '계산하기', '메아리', '가격', '수량']
-};
-const trans_function = {
-    'result': {
-        'English': function (meari, n, cost) {
-            return `Total ${meari} Echoes (+${meari-n}) for ${cost}won`
+const menu_meari = [60, 194, 320, 723, 2123, 3498, 6918]
+const menu_string = ["60 + 0", "185 + 9", "305 + 15", "690 + 33", "2025 + 98", "3330 + 168", "6590 + 328"]
+const l = menu_meari.length;
+
+const trans = {
+    'English': {
+        'string': ['Identity V Echoes calculator', 'How much Echoes do you need?', 'Calculate', 'Echoes', 'Cost', 'Amount'],
+        'result': function (meari, n, cost) {
+            return `Total ${meari} Echoes (+${meari-n}) for ${cost}USD`
         },
-        'Korean': function (meari, n, cost) {
-            return `${cost}원에 총 ${meari} 메아리 (+${meari-n})`
-        }
+        'invalid': 'Please enter the natural number',
+        'menu_price': [0.99, 2.99, 4.99, 9.99, 29.99, 49.99, 99.99],
+        'decimal': 2,
+        'confirm': 'More than 999,999 Echoes can be slow'
     },
-    'invalid': {
-        'English': 'Please enter the natural number',
-        'Korean': '자연수를 입력해 주세요'
+    'Korean': {
+        'string': ['제5인격 메아리 계산기', '메아리가 얼마나 필요하신가요?', '계산하기', '메아리', '가격', '수량'],
+        'result': function (meari, n, cost) {
+            return `${cost}원에 총 ${meari} 메아리 (+${meari-n})`
+        },
+        'invalid': '자연수를 입력해 주세요',
+        'menu_price': [1200, 3900, 5900, 14000, 39000, 65000, 119000],
+        'decimal': 0,
+        'confirm': '백만 이상의 메아리는 느릴 수도 있어요'
     }
-};
+}
 
 function optimize(n) {
     var data = Array(n + menu_meari[l - 1] + 1).fill(null).map(() => [Array(l).fill(Infinity), Infinity]);
@@ -37,46 +40,57 @@ function optimize(n) {
         for (let i = 0; i < l; i++) {
             var comb_new = comb.slice();
             comb_new[i] += 1;
-            var cost_new = cost + menu_price[i];
+            var cost_new = cost + trans[language]['menu_price'][i];
             if (cost_new <= data[current + menu_meari[i]][1]) {
                 data[current + menu_meari[i]] = [comb_new, cost_new]
             }
         }
     }
-
     var solution = data.slice(n, n + menu_meari[l - 1] + 1).sort((a, b) => a[1] - b[1])[0];
+    solution[1] = solution[1].toFixed(trans[language]['decimal']);
     var meari = 0;
     solution[0].forEach((elem, index) => {
         meari += elem * menu_meari[index]
     });
-    return [solution[0], solution[1],
+    return [
+        solution[0],
+        solution[1],
         [meari]
     ]
 }
 
-function process() {
+function process(number = null) {
     var value = document.getElementById('input').value
     var n = parseInt(value)
     if (!isNaN(n) && n >= 0 && n === parseFloat(value)) {
+        if (n >= 1000000) {
+            var accept = confirm(trans[language]['confirm']);
+            if (!(accept)) {
+                return
+            }
+        }
         var [comb, cost, meari] = optimize(n);
         [memo_meari, memo_n, memo_cost] = [meari, n, cost];
-        document.getElementById('result').innerHTML = trans_function['result'][trans_language](meari, n, cost);
-        var table = document.getElementById('table')
-        var rows = table.rows;
+        document.getElementById('result').innerHTML = trans[language]['result'](meari, n, cost);
+        var rows = document.getElementById('table').rows;
         for (var i = 0; i < rows.length - 1; i++) {
             rows[i + 1].cells[2].innerHTML = comb[i];
         }
     } else {
-        alert(trans_function['invalid'][trans_language]);
+        alert(trans[language]['invalid']);
     }
 }
 
 function translate(from, to) {
-    var from_string = trans_string[from];
-    var to_string = trans_string[to];
+    var from_string = trans[from]['string'];
+    var to_string = trans[to]['string'];
     const elements = document.getElementsByClassName('string');
     for (let element of elements) {
         element.innerHTML = to_string[from_string.indexOf(element.innerHTML)];
+    }
+    var rows = document.getElementById('table').rows;
+    for (var i = 0; i < rows.length - 1; i++) {
+        rows[i + 1].cells[1].innerHTML = trans[to]['menu_price'][i];
     }
     document.getElementById('content').className = `content full-screen ${to}`;
     document.getElementById('submit').className = `center_inner string ${to}`;
@@ -84,9 +98,6 @@ function translate(from, to) {
     var input = document.getElementById('input');
     input.className = `center_inner ${to}`;
     input.setAttribute('placeholder', to_string[from_string.indexOf(input.getAttribute('placeholder'))]);
-    if (!(memo_meari === null || memo_n === null || memo_cost)) {
-        document.getElementById('result').innerHTML = trans_function['result'][to](memo_meari, memo_n, memo_cost);
-    }
 }
 
 window.onload = () => {
@@ -96,19 +107,28 @@ window.onload = () => {
         for (let j = 0; j < 3; j++) {
             let cell = row.insertCell(j);
             if (j == 0) {
-                cell.innerHTML = string_meari[i];
+                cell.innerHTML = menu_string[i];
             } else if (j == 1) {
-                cell.innerHTML = menu_price[i];
+                cell.innerHTML = trans[language]['menu_price'][i];
             } else {
                 cell.innerHTML = 0;
             }
         }
     }
     document.getElementById('language').onchange = function () {
-        var language = this.children[this.selectedIndex].innerHTML.trim();
-        if (trans_language !== language) {
-            translate(trans_language, language);
-            trans_language = language;
+        var selectedLanguage = this.children[this.selectedIndex].innerHTML.trim();
+        if (language !== selectedLanguage) {
+            translate(language, selectedLanguage);
+            language = selectedLanguage;
+            input.value = memo_n;
+            var [comb, cost, meari] = optimize(memo_n);
+            [memo_meari, memo_cost] = [meari, cost];
+            console.log(trans[language]['result'](meari, memo_n, cost), 'translate', language);
+            document.getElementById('result').innerHTML = trans[language]['result'](meari, memo_n, cost);
+            var rows = document.getElementById('table').rows;
+            for (var i = 0; i < rows.length - 1; i++) {
+                rows[i + 1].cells[2].innerHTML = comb[i];
+            }
         }
     }
 }
