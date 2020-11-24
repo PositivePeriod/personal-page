ed3ee84567d84251cfa2de09e58ed218a8cb6558
PositivePeriod <positiveperiod.dev@gmail.com>
2020-11-24 19:16:20 +0900

    Update project
    Text beautify

--- public/develop/Text beautify/source.js
+++ public/develop/Text beautify/source.js
@@ -7,28 +7,41 @@ function beautify(content) {
     content = content.replace(/\n/g, '  ');*/
 
     // 2. Sentence classify
-    content = content.replace(/\. /g, '\.\n'); // default
-    content = content.replace(/: /g, ':\n'); // additional content or equation
-    content = content.replace(/; /g, ';\n'); // additional content or equation
+    content = content.replace(/\. +/g, '\.\n'); // default
+    content = content.replace(/:(?=[^ ])/g, ': \n'); // additional content or equation
+    content = content.replace(/;(?=[^ ])/g, '; '); // additional content or equation
+    content = content.replace(/; +/g, ';\n'); // additional content or equation
 
     // var integerRegex = '/^[0-9]+$/'
     content = content.replace(/\(([0-9]+)\.([0-9]+)\)/g, (match) => (match + '\n')); // equation; Ex) (2.16)
 
     // except some cases like Fig.1.1. about dot
     //  Ex) Fig.1.2, Mr.Steven, pp. 58 - 70
-    var exception_1 = ['Fig', 'lbs', 'Mr', 'Mrs', 'Ms', 'Eq', 'Eqs', 'pp', 'Vol', 'Pt'];
-    exception_1.forEach(word => {
-        content = content.replace(`/${word}\.\n/g`, `${word}\.`)
+    const daily_single_abbr = ['Fig', 'ft', 'lb', 'lbs', 'Mr', 'Mrs', 'Ms', 'Jr', 'Eq', 'Eqs', 'pp', 'Vol', 'Pt', 'Dr', 'Dept', 'par', 'Mt', 'govt', 'Bros', 'Oxf'];
+    const case_insensitive_abbr = ['Tel', 'Govt', 'Inc', 'No'];
+    const time_unit_abbr = ['sec', 'min', 'hr', 'wk', 'mo', 'yr'];
+    const day_abbr = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'];
+    const month_abbr = ['Jan', 'Feb', 'Mar', 'Apr', 'Aug', 'Sep', 'Sept', 'Oct', 'Nov', 'Dec'];
+    const single_period_abbr = [...daily_single_abbr, ...case_insensitive_abbr, ...case_insensitive_abbr.map(x => x.toLowerCase()), ...time_unit_abbr, ...day_abbr, ...month_abbr];
+    single_period_abbr.forEach(word => {
+        content = content.replace(`/${word}\.\n/g`, `${word}\.`);
+    });
+
+    const daily_multiple_abbr = ['a.m.', 'A.M.', 'p.m.', 'P.M.', 'A.D.', 'B.C.', 'B.C.E.', 'C.E.', 'Q.E.D.', 'Ph.D.', 'P.E.', 'p.p.', 'P.P.'];
+    const location_abbr = ['U.N.', 'U.S.A.', 'U.S.', 'L.A.', 'U.K.', 'N.Z.'];
+    const multiple_period_abbr = [...daily_multiple_abbr, ...location_abbr];
+    multiple_period_abbr.forEach(word => {
+        content = content.replace(`/${word}\n/g`, `${word}`);
     });
 
     // Ex) Fig.1.2, Mr.Steven, pp. 58 - 70
-    content = content.replace(/(\.[0-9]+\.)\n/g, (match, p1) => (p1))
+    content = content.replace(/(\.[0-9]+\.)\n/g, (match, p1) => (p1));
 
     // 3. little grammar error - except some cases like 1. or Mr.about dot
     // int
     // Ex) 1. one content, 2. another content
-    content = content.replace(/(\n[0-9]+\.)\n/g, (match, p1) => (p1))
-    content = content.replace(/( [0-9]+\.)\n/g, (match, p1) => (p1))
+    content = content.replace(/(\n[0-9]+\.)\n/g, (match, p1) => (p1));
+    content = content.replace(/( [0-9]+\.)\n/g, (match, p1) => (p1));
 
     // TODO - is it right in the logic
     // alphabet A ~ Z, a ~ z
@@ -43,19 +56,24 @@ function beautify(content) {
     // Ex) Fig.4.1a-c.*/
 
     // 4. Clean 
-    content = content.replace(/ +/g, ' ') // '     ' -> ' '
-    content = content.replace(/( ?)+\n+( ?)+/g, '\n') // '    \n\n\n ' -> '\n'
-    content = content.replace(/ +\./g, '.') // '     .' -> '.'
-    content = content.replace(/ +,/g, ',') // '     ,' -> ','
-    content = content.replace(/,[^ ]/g, ', ') // ',else' -> ', else'
+    content = content.replace(/ +/g, ' '); // '     ' -> ' '
+    content = content.replace(/( ?)+\n+( ?)+/g, '\n'); // '    \n\n\n ' -> '\n'
+    content = content.replace(/ +\./g, '.'); // '     .' -> '.'
+    content = content.replace(/ +,/g, ','); // '     ,' -> ','
+    content = content.replace(/,[^ ]/g, ', '); // ',else' -> ', else'
 
     // 5. Finish
     return content
 }
 
-function adjustHeight(e) {
-    e.target.style.height = "1px";
-    e.target.style.height = e.target.scrollHeight + "px";
+function adjustHeights(event) {
+    var input = document.getElementById('input');
+    input.style.height = "1px";
+    input.style.height = input.scrollHeight + "px";
+
+    var output = document.getElementById('output');
+    output.style.height = "1px";
+    output.style.height = output.scrollHeight + "px";
 }
 
 function check() {
@@ -63,16 +81,25 @@ function check() {
     var output = document.getElementById('output');
     output.value = beautify(content);
     output.select();
-    adjustHeight({target: output})
+    adjustHeights();
+    window.scroll({
+        top: 0, 
+        left: 0, 
+        behavior: 'smooth'
+      });
 }
 window.onload = () => {
     // Textarea changing height
-    document.getElementById('input').addEventListener('input', adjustHeight);
+    document.getElementById('input').addEventListener('input', adjustHeights);
+    window.addEventListener("resize", adjustHeights, false);
 
     // Check with enter
-    document.addEventListener("keypress", function onEvent(event) {
-        if (event.key === "Enter") {
-            check();
+    document.addEventListener("keydown", function onEvent(event) {
+        if (event.key == 'Enter') {
+            if (!event.shiftKey) {
+                event.preventDefault();
+                check();
+            };
         };
     });
 
